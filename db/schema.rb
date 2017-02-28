46ef1140851dd949286f5f508b541386b5c0aa17
Meredith Jones <meredithjw@gmail.com>
2017-02-27 21:23:32 -0800

    Create migration to add cups per sample column to cuppings tablef

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228035856) do
+ActiveRecord::Schema.define(version: 20170228052119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,10 @@ ActiveRecord::Schema.define(version: 20170228035856) do
   create_table "cuppings", force: :cascade do |t|
     t.string   "location"
     t.datetime "cup_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "host_id"
+    t.integer  "cups_per_sample"
     t.index ["host_id"], name: "index_cuppings_on_host_id", using: :btree
   end
 
