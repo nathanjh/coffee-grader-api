fcac627d97b1e35614c91ad2366fed1c279d9930
Meredith Jones <meredithjw@gmail.com>
2017-02-27 20:54:50 -0800

    Add validation and association tests for scores model, add validations and associations to model. Tests passing. TODO: may want to add more tests regarding the value of the data.

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,8 @@
 class Score < ApplicationRecord
+  belongs_to :grader, foreign_key: "grader_id", class_name: "User"
   belongs_to :cupped_coffee, optional: true
-  belongs_to :user, optional: true
   belongs_to :cupping
+
+  validates_presence_of :grader_id, :cupping_id, :cupped_coffee_id, :aroma, :aftertaste, :acidity, :body, :uniformity, :balance, :clean_cup, :sweetness, :overall, :defects, :total_score
+
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Score, type: :model do
     @user = create(:user)
     @cupping = create(:cupping, host_id: @user.id)
     @cupped_coffee = create(:cupped_coffee, coffee_id: @coffee.id, roaster_id: @roaster.id, cupping_id: @cupping.id)
-    @score = create(:score, grader_id: @user.id, cupping_id: @cupping.id)
+    @score = create(:score, grader_id: @user.id, cupping_id: @cupping.id, cupped_coffee_id: @cupped_coffee.id)
   end
 
   describe 'attributes' do
@@ -28,7 +28,32 @@ RSpec.describe Score, type: :model do
     it { should respond_to(:cupped_coffee_id) }
     it { should respond_to(:cupping_id) }
     it { should respond_to(:grader_id) }
-
   end
 
+  describe 'validations' do
+    subject { score }
+
+    it { should validate_presence_of(:grader_id) }
+    it { should validate_presence_of(:cupping_id) }
+    it { should validate_presence_of(:cupped_coffee_id) }
+    it { should validate_presence_of(:aroma) }
+    it { should validate_presence_of(:aftertaste) }
+    it { should validate_presence_of(:acidity) }
+    it { should validate_presence_of(:body) }
+    it { should validate_presence_of(:uniformity) }
+    it { should validate_presence_of(:balance) }
+    it { should validate_presence_of(:clean_cup) }
+    it { should validate_presence_of(:sweetness) }
+    it { should validate_presence_of(:overall) }
+    it { should validate_presence_of(:defects) }
+    it { should validate_presence_of(:total_score) }
+  end
+
+  describe 'associations' do
+    subject { score }
+
+    it { should belong_to(:grader) }
+    it { should belong_to(:cupped_coffee) }
+    it { should belong_to(:cupping) }
+  end
 end
