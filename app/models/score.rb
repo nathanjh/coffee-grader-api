b80e22eeb8487fa2f25de79409a979cd1452f35e
Meredith Jones <meredithjw@gmail.com>
2017-02-27 21:14:44 -0800

    Look into tests for values of attributes

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,5 +4,4 @@ class Score < ApplicationRecord
   belongs_to :cupping
 
   validates_presence_of :grader_id, :cupping_id, :cupped_coffee_id, :aroma, :aftertaste, :acidity, :body, :uniformity, :balance, :clean_cup, :sweetness, :overall, :defects, :total_score
-
 end
