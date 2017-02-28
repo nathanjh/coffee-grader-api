d274befb34b89fa49d9e5a8bba81b4bacf776970
Nathan Herrera <nathanherrera@gmail.com>
2017-02-27 18:34:09 -0800

    Add basic scores model/ migrations TODO: tests

---
+++ spec/factories/scores.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :score do
+    cupped_coffee nil
+    grader nil
+    cupping nil
+    roast_level 1
+    aroma "9.99"
+    aftertaste "9.99"
+    acidity "9.99"
+    body "9.99"
+    uniformity "9.99"
+    balance "9.99"
+    clean_cup "9.99"
+    sweetness "9.99"
+    overall "9.99"
+    defects 1
+    total_score "9.99"
+    notes "MyText"
+  end
+end
