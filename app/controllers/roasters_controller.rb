74bc333d09aec3441a966dffeb9d11213ecbb83d
Meredith Jones <meredithjw@gmail.com>
2017-03-10 14:16:55 -0800

    Add exception handler concern and include exception handler in application controller.

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -1,25 +1,44 @@
 class RoastersController < ApplicationController
-  before_action :find_roaster, only: [:show, :patch, :delete]
+  before_action :find_roaster, only: [:show, :update, :destroy]
 
+
+  # GET /roasters
+  def index
+    @roasters = Roaster.all
+    json_response(@roasters)
+  end
 
   # GET /roasters/id
   def show
     json_response(@roaster)
   end
 
+  # POST /roasters
+  def create
+    @roaster = Roaster.create!(roaster_params)
+    json_response(@roaster, :created)
+  end
 
+  # PATCH /roasters/:id
+  def update
+    @roaster.update(roaster_params)
+    head :no_content
+  end
 
+  # DELETE /roasters/:id
+  def destroy
+    @roaster.destroy
+    head :no_content
+  end
 
   private
 
   def find_roaster
     @roaster = Roaster.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Roaster does not exist' }, status: :not_found
   end
 
   def roaster_params
-    params.require(:roaster).permit(:name, :location, :website)
+    params.permit(:name, :location, :website)
   end
 
 end
