78eac9133e1da9a0594aa3efac39d25602dca328
Nathan Herrera <nathanherrera@gmail.com>
2017-03-09 20:04:37 -0800

    Add request spec helper to parse JSON response, as well as requests spec directory

---
+++ spec/requests/coffees_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Coffees API', type: :request do
+  describe 'GET /coffees' do
+
+  end
+
+  describe 'GET /coffees/:id' do
+
+  end
+
+  describe 'POST /coffees' do
+
+  end
+
+  describe 'PUT /coffees/:id' do
+
+  end
+
+  describe 'DELETE /coffees/:id' do
+
+  end
+end
