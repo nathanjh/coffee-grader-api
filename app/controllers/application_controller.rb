0302e0cc9fe7f65e243e970ab645494e7b2aa44e
Nathan Herrera <nathanherrera@gmail.com>
2017-03-14 00:43:55 -0700

    Sanitize username, name params for devise user model, configure development env for actionmailer and mailcatcher (for testing) to support email confirmation on sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Response
   include ExceptionHandler
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+  end
 end
--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -45,4 +45,7 @@ DeviseTokenAuth.setup do |config|
   # If, however, you wish to integrate with legacy Devise authentication, you can
   # do so by enabling this flag. NOTE: This feature is highly experimental!
   # config.enable_standard_devise_support = false
+
+  # Set default confirm success url for email confirmation
+  config.default_confirm_success_url = '/'
 end
