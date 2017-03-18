bcc21a6ed2c4189494a504c578b6414678da72b7
Nathan Herrera <nathanherrera@gmail.com>
2017-03-18 12:25:07 -0700

    Add controller specs for cupped_coffees

---
+++ spec/controllers/cupped_coffees_controller_spec.rb
@@ -0,0 +1,103 @@
+require 'rails_helper'
+
+RSpec.describe CuppedCoffeesController, type: :controller do
+  let(:cupping) { create(:cupping) }
+  let(:coffee) { create(:coffee) }
+  let(:roaster) { create(:roaster) }
+  let(:cupped_coffees) { create_list(:cupped_coffees, 5, cupping_id: cupping.id) }
+  let(:cupped_coffee) { cupped_coffees.first }
+
+  describe 'GET #index' do
+    it 'collects all cuppeded coffees into @cupped_coffees' do
+      get :index, params: { cupping_id: cupping.id }, format: :json
+      expect(assigns(:cupped_coffees)).to eq cupped_coffees
+    end
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested cupped_coffee as @cupped_coffee' do
+      get :show, params: { id: cupped_coffee, cupping_id: cupping.id },
+                 format: :json
+      expect(assigns(:cupped_coffee)).to eq cupped_coffee
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with vaild attributes' do
+      it 'saves a new cupped_coffee in the database' do
+        expect do
+          post :create, params: {
+            cupped_coffee: attributes_for(:cupped_coffee,
+                                          cupping_id: cupping.id,
+                                          coffee_id: coffee.id,
+                                          roaster_id: roaster.id),
+            cupping_id: cupping.id
+          }, format: :json
+        end
+          .to change(CuppedCoffee, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it "doesn't save the new coffee in the database" do
+        expect do
+          post :create, params: {
+            cupped_coffee: attributes_for(:cupped_coffee),
+            cupping_id: cupping.id
+          }, format: :json
+        end
+          .not_to change(CuppedCoffee, :count)
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with vaild attributes' do
+      it 'locates the requested cupped_coffee' do
+        patch :update, params: { id: cupped_coffee,
+                                 cupping_id: cupping.id,
+                                 cupped_coffee: attributes_for(:cupped_coffee) },
+                       format: :json
+        expect(assigns(:cupped_coffee)).to eq(cupped_coffee)
+      end
+
+      it "updates the cupped_coffee's attributes" do
+        patch :update, params: { id: cupped_coffee,
+                                 cupping_id: cupping.id,
+                                 cupped_coffee: { coffee_alias: 'Uptown!' } },
+                       format: :json
+        cupped_coffee.reload
+        expect(cupped_coffee.coffee_alias).to eq('Uptown!')
+      end
+    end
+
+    context 'with invalid attributes' do
+      it "doesn't change the cupped_coffee's attributes" do
+        # let's update an existing record's coffee_alias to test cupping-scoped
+        # uniqueness vailidity
+        patch :update, params: { id: cupped_coffee,
+                                 cupping_id: cupping.id,
+                                 cupped_coffee: { coffee_alias: 'Once-a-Cupping' } },
+                       format: :json
+        # record is invalid when duplicate alias is used in the same cupping
+        cupped_coffee2 = cupped_coffees.last
+        patch :update, params: { id: cupped_coffee2,
+                                 cupping_id: cupping.id,
+                                 cupped_coffee: { coffee_alias: 'Once-a-Cupping' } },
+                       format: :json
+        expect(cupped_coffee2.coffee_alias).not_to eq('Once-a-Cupping')
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'deletes the coffee record from the database' do
+      expect do
+        delete :destroy, params: { id: cupped_coffee.id,
+                                   cupping_id: cupping.id },
+                         format: :json
+      end
+        .to change(CuppedCoffee, :count).by(-1)
+    end
+  end
+end
