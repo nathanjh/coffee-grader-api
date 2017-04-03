6d20998434746091e9b6f50a7380c5e63d590816
Nathan Herrera <nathanherrera@gmail.com>
2017-04-03 13:19:20 -0700

    Refactors import class method, extracting private class helper methods.

--- app/models/score.rb
+++ app/models/score.rb
@@ -20,31 +20,13 @@ class Score < ApplicationRecord
                         :total_score,
                         :final_score
 
-  # In case we run into performance issues with creating multiple scores...
-  # it'll be interesting to test either way...
   # Database constraints ensure that no bad scores are saved
   def self.import(scores)
     return unless scores.any?
+    values = score_values(scores)
 
-    values = scores.map do |score|
-      sanitize_sql_array(
-        ['(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-         score.cupped_coffee_id, score.cupping_id, score.roast_level,
-         score.aroma, score.aftertaste, score.acidity, score.body,
-         score.uniformity, score.balance, score.clean_cup, score.sweetness,
-         score.overall, score.defects, score.total_score, score.notes,
-         DateTime.now, DateTime.now, score.grader_id, score.final_score,
-         score.flavor]
-      )
-    end
-
-    query = <<-SQL
-    SAVEPOINT batch_insert_savepoint;
-    INSERT INTO scores (cupped_coffee_id, cupping_id, roast_level, aroma, aftertaste, acidity, body, uniformity, balance, clean_cup, sweetness, overall, defects, total_score, notes, created_at, updated_at, grader_id, final_score, flavor) VALUES
-    #{values.join(',')}
-    SQL
     begin
-      connection.execute(query)
+      connection.execute(build_query(values))
     rescue ActiveRecord::StatementInvalid
       connection.execute('ROLLBACK TO SAVEPOINT batch_insert_savepoint')
       raise BatchInsertScoresError
@@ -56,8 +38,40 @@ class Score < ApplicationRecord
   # Exception class to handle bulk import errors--passed off to ExceptionHandler
   # controller concern to generate json response.
   class BatchInsertScoresError < ActiveRecord::StatementInvalid
-    def initialize(msg = 'There was a probelm with one or more of the score parameters. Unable to save scores to the database.')
+    ERROR_MESSAGE = <<-MSG
+    There was a probelm with one or more of the score parameters. Unable to save scores to the database.
+    MSG
+                    .freeze
+
+    def initialize(msg = ERROR_MESSAGE)
       super
+    end
+  end
+
+  # to add private class helper methods for Score
+  class << self
+    private
+
+    def score_values(scores)
+      scores.map do |score|
+        sanitize_sql_array(
+          ['(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+           score.cupped_coffee_id, score.cupping_id, score.roast_level,
+           score.aroma, score.aftertaste, score.acidity, score.body,
+           score.uniformity, score.balance, score.clean_cup, score.sweetness,
+           score.overall, score.defects, score.total_score, score.notes,
+           DateTime.now, DateTime.now, score.grader_id, score.final_score,
+           score.flavor]
+        )
+      end
+    end
+
+    def build_query(values)
+      <<-SQL
+      SAVEPOINT batch_insert_savepoint;
+      INSERT INTO scores (cupped_coffee_id, cupping_id, roast_level, aroma, aftertaste, acidity, body, uniformity, balance, clean_cup, sweetness, overall, defects, total_score, notes, created_at, updated_at, grader_id, final_score, flavor) VALUES
+      #{values.join(',')}
+      SQL
     end
   end
 end
