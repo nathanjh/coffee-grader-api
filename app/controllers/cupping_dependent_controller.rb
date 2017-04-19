a3b973a50ee5973b0ee53004af99685410c94ed6
Nathan Herrera <nathanherrera@gmail.com>
2017-04-18 18:15:41 -0700

    Adds more descriptive error message for closed-cupping-restriction

--- app/controllers/cupping_dependent_controller.rb
+++ app/controllers/cupping_dependent_controller.rb
@@ -28,26 +28,14 @@ class CuppingDependentController < CoffeeGraderApiController
   end
 
   def check_cupping_status
-    # add logic to find a cupping by params (otherwise, locate by requested
-    # resource's cupping_id), then check open status and render
-    # an appropriate response if @cupping.open == false
-    # if a rescource is passed in, find cupping by resource.cupping_id, otherwise,
-    # find cupping using cupping_id in params
-    # if resource
-    #   cupping = Cupping.find(resource.cupping_id)
-    # elsif params[:cupping_id]
-    #   cupping = Cupping.find(params[:cupping_id])
-    # end
-    # render json: { error: 'Cupping is closed. Request cannot be completed.' },
-    #        status: :bad_request unless cupping.open
     @cupping ||=
       if params[:cupping_id]
         Cupping.find(params[:cupping_id])
       elsif @score
         Cupping.find(@score.cupping_id)
       end
-
-    json_response({ message: 'Cupping is closed.' }, :bad_request) if
+    error_message = 'Cupping is closed and cannot receive any new invites, coffee samples, or scores.'
+    json_response({ error: error_message }, :bad_request) if
       @cupping && !@cupping.open
   end
 end
