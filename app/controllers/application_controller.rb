a86c2686e6131d0dc7af3a888ad0564fc0f0e39a
Nathan Herrera <nathanherrera@gmail.com>
2017-07-06 12:23:43 -0700

    Sanitize invite_token for devise user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:username, :name, :invite_token])
   end
 end
