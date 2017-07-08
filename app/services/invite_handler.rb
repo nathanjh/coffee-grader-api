fe4e789d927501209b5a044e4688d90fe9b6b46f
Nathan Herrera <nathanherrera@gmail.com>
2017-07-08 01:57:29 -0700

    Fixes bug where invite_mailer_spec was persisting records in the test database (causing other tests to fail). All green

--- app/services/invite_handler.rb
+++ app/services/invite_handler.rb
@@ -6,7 +6,6 @@ class InviteHandler
   def call(invite)
     if invite.grader_id.blank?
       invite.update!(invite_token: generate_token)
-      # to interact with dbs from the mailer)
       # call mailer service (no user account for grader)
     else
       # call mailer service (user account for grader)
--- spec/mailers/invite_mailer_spec.rb
+++ spec/mailers/invite_mailer_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe InviteMailer, type: :mailer do
-  # let!(:cupping) { create(:cupping) }
-  # let(:guest_invite) { cupping.invites.create!(grader_email: 'liam@s.com') }
-  # let(:guest_email) { InviteMailer.guest_invitation(guest_invite, cupping) }
-  # let(:user_invite) { cupping.invites.create!(grader_id: create(:user).id) }
-  # let(:user_email) { InviteMailer.user_invitation(user_invite, cupping) }
+  let!(:cupping) { create(:cupping) }
+  let(:guest_invite) { cupping.invites.create!(grader_email: 'liam@s.com') }
+  let(:guest_email) { InviteMailer.guest_invitation(guest_invite, cupping) }
+  let(:user_invite) { cupping.invites.create!(grader_id: create(:user).id) }
+  let(:user_email) { InviteMailer.user_invitation(user_invite, cupping) }
 
-  before(:context) do
-    cupping = create(:cupping)
-    invite = cupping.invites.create!(grader_email: 'newfriend@whatever.com')
-    @mail = InviteMailer.guest_invitation(invite, cupping)
-  end
+  # before(:context) do
+  #   cupping = create(:cupping)
+  #   invite = cupping.invites.create!(grader_email: 'newfriend@whatever.com')
+  #   @mail = InviteMailer.guest_invitation(invite, cupping)
+  # end
   after(:context) { ActionMailer::Base.deliveries.clear }
 
   describe '#guest_invitation' do
     it 'sends the email' do
-      @mail.deliver_now
+      guest_email.deliver_now
       expect(ActionMailer::Base.deliveries.count).to eq 1
     end
   end
