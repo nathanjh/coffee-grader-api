ad4275214f28a35c061b773e9b3f398f5668931c
Nathan Herrera <nathanherrera@gmail.com>
2017-07-08 14:14:23 -0700

    Adds specs/implements InviteMailer#user_invitation renders headers and body(passing) - also refactors invite_mailer views to use partial for shared content

--- spec/mailers/invite_mailer_spec.rb
+++ spec/mailers/invite_mailer_spec.rb
@@ -33,6 +33,25 @@ RSpec.describe InviteMailer, type: :mailer do
   end
 
   describe '#user_invitation' do
+    before(:example) do
+      user_email.deliver_now
+      @delivered_mail = ActionMailer::Base.deliveries.first
+    end
+    it 'sends the email' do
+      expect(@delivered_mail).to be_a(Mail::Message)
+    end
 
+    it 'renders the headers' do
+      expect(@delivered_mail.subject).to match(/invited you to a cupping/)
+      expect(@delivered_mail.to).to eq([User.find(user_invite.grader_id).email])
+      expect(@delivered_mail.from).to eq([InviteMailer.default[:from]])
+    end
+
+    it 'renders the body' do
+      user_account_link_re =
+        %r{https:\/\/www.coffeegrader.com\/users\/#{user_invite.grader_id}}
+      expect(@delivered_mail.body.encoded)
+        .to match(user_account_link_re)
+    end
   end
 end
--- spec/mailers/previews/invite_mailer_preview.rb
+++ spec/mailers/previews/invite_mailer_preview.rb
@@ -2,7 +2,16 @@
 class InviteMailerPreview < ActionMailer::Preview
   def guest_invitation
     cupping = Cupping.last
-    invite = cupping.invites.first
+    # ensure that we have a guest_invite to use...
+    invite =
+      cupping.invites.build(grader_email: Faker::Internet.unique.email,
+                            invite_token: SecureRandom.urlsafe_base64(16))
     InviteMailer.guest_invitation(invite, cupping)
+  end
+
+  def user_invitation
+    cupping = Cupping.first
+    invite = cupping.invites.first
+    InviteMailer.user_invitation(invite, cupping)
   end
 end
