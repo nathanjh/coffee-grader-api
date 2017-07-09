1c509282ce662072ea66d0d70fc6cbd4c8f52182
Nathan Herrera <nathanherrera@gmail.com>
2017-07-09 16:41:20 -0700

    Adds user_invite_contact service and specs(passing)

---
+++ spec/services/invite_handler/user_invite_contact_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+include ActiveJob::TestHelper
+
+RSpec.describe InviteHandler::UserInviteContact do
+  context 'when contacting via email(async)' do
+    before do
+      ActiveJob::Base.queue_adapter = :test
+      @service = described_class.build
+      @cupping = create(:cupping)
+      @invite = @cupping.invites.create(grader_id: create(:user).id)
+    end
+
+    it 'adds a job to the mailers queue' do
+      expect { @service.call(@invite, @cupping) }
+        .to have_enqueued_job.on_queue('mailers')
+    end
+
+    it 'delivers the email' do
+      inbox = ActionMailer::Base.deliveries
+      expect do
+        perform_enqueued_jobs { @service.call(@invite, @cupping) }
+      end
+        .to change(inbox, :length).by 1
+    end
+  end
+end
