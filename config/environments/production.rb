f8049af8f6174de1107f64be0e1811521d1e4023
Nathan Herrera <nathanherrera@gmail.com>
2017-07-15 00:59:08 -0700

    Adds sendgrid configuration for actionmailer(production env) and heroku procfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "coffee-grader-api_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Configure sendgrid for email delivery
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
