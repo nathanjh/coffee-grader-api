0f51f3992a0be848c6ad5ce1b97119afa6afa99a
Nathan Herrera <nathanherrera@gmail.com>
2017-07-15 15:54:30 -0700

    Adds production seedfile, along with data files

--- db/seeds/seed_production.rb
+++ db/seeds/seed_production.rb
@@ -1,1 +1,9 @@
 # Seedfile loaded in production environment
+require 'csv'
+# Seed coffees
+coffees_file = File.join(Rails.root, 'db', 'seeds', 'coffees.csv')
+CSV.foreach(coffees_file, headers: true) { |coffee| Coffee.create(coffee.to_h) }
+
+# Seed roasters
+roasters_file = File.join(Rails.root, 'db', 'seeds', 'roasters.csv')
+CSV.foreach(roasters_file, headers: :true) { |roaster| Roaster.create(roaster.to_h) }
