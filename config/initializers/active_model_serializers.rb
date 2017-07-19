0411e5d97edddabf5a72784b17f71d3314026450
Nathan Herrera <nathanherrera@gmail.com>
2017-07-19 01:26:47 -0700

    Installs active-model-serializers gem, config for json api, adds coffee serializer

---
+++ config/initializers/active_model_serializers.rb
@@ -0,0 +1,5 @@
+# Configure Active Model Serializers adapter, etc.
+# ActiveModelSerializers.config do |config|
+#   config.adapter = :json_api
+# end
+ActiveModelSerializers.config.adapter = :json_api
